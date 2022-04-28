275a671ff6373f7c4b7461dc97523f16bc7dbaf3
Flexberry-man <mail@flexberry.net>
2022-04-28 05:22:22 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -22,13 +22,13 @@ export default Controller.extend({
             link: 'i-i-s-zayavki-клиент-l',
             caption: i18n.t('forms.application.sitemap.обработка-заявок.i-i-s-zayavki-клиент-l.caption'),
             title: i18n.t('forms.application.sitemap.обработка-заявок.i-i-s-zayavki-клиент-l.title'),
-            icon: 'chart bar',
+            icon: 'chart line',
             children: null
           }, {
             link: 'i-i-s-zayavki-заявка-l',
             caption: i18n.t('forms.application.sitemap.обработка-заявок.i-i-s-zayavki-заявка-l.caption'),
             title: i18n.t('forms.application.sitemap.обработка-заявок.i-i-s-zayavki-заявка-l.title'),
-            icon: 'archive',
+            icon: 'suitcase',
             children: null
           }]
         }
